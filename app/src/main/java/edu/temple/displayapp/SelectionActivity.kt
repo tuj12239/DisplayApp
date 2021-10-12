c474d2a79670e95b7b087476e1215abe5c0ce07b
Isaac <tuj12239@temple.edu>
2021-10-12 10:14:48 -0400

    Replaces hardcoded strings with resources

--- app/src/main/java/edu/temple/displayapp/SelectionActivity.kt
+++ app/src/main/java/edu/temple/displayapp/SelectionActivity.kt
@@ -1,11 +1,7 @@
 package edu.temple.displayapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,20 +22,21 @@ class SelectionActivity : AppCompatActivity() {
     }
 
     private fun getImageArray(): Array<ImageClass> {
+        val names: Array<String> = resources.getStringArray(R.array.flags)
 
         return arrayOf(
-            ImageClass("Donphan", R.drawable.brazil),
-            ImageClass("Dunsparce", R.drawable.canada),
-            ImageClass("Eevee", R.drawable.france),
-            ImageClass("Gengar", R.drawable.germany),
-            ImageClass("Heracross", R.drawable.ireland),
-            ImageClass("Magikarp", R.drawable.japan),
-            ImageClass("Magnezone", R.drawable.mexico),
-            ImageClass("Medicham", R.drawable.phillippines),
-            ImageClass("Mudkip", R.drawable.southkorea),
-            ImageClass("Murkrow", R.drawable.spain),
-            ImageClass("Prinplup", R.drawable.unitedkingdom),
-            ImageClass("Quagsire", R.drawable.unitedstates)
+            ImageClass(names[0], R.drawable.brazil),
+            ImageClass(names[1], R.drawable.canada),
+            ImageClass(names[2], R.drawable.france),
+            ImageClass(names[3], R.drawable.germany),
+            ImageClass(names[4], R.drawable.ireland),
+            ImageClass(names[5], R.drawable.japan),
+            ImageClass(names[6], R.drawable.mexico),
+            ImageClass(names[7], R.drawable.philippines),
+            ImageClass(names[8], R.drawable.southkorea),
+            ImageClass(names[9], R.drawable.spain),
+            ImageClass(names[10], R.drawable.unitedkingdom),
+            ImageClass(names[11], R.drawable.unitedstates)
         )
     }
 }
